74ab337d9e34aa73829188927386231ded7c24bb
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-26 00:35:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,4 @@
-#This method below uses only the .each array method ONLY. Here, we have our method #square_array which we want to use only the .each in the logic to capture the squared numbers/elements (since we can't use .collect, .inject in this method). I have created an empty array named squared_numbers and using the block {} to capture each element from (array), square the value and place it inside the squared_numbers array. 
+#This method below uses only the .each array method ONLY. Here, we have our method #square_array which we want to use only the .each in the logic to capture the squared numbers/elements (since we can't use .collect, .inject in this method). I have created an empty array named squared_numbers and using the block {} to capture each element from (array). The valuable in the pipes "|num|" square the value and place it inside the squared_numbers array. 
 
 def square_array(array)
   squared_numbers = []
