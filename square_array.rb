6af0fc6ccf66e5dd58ee0fd20ebbf584449c83ff
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-26 00:44:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,4 @@
-#This array method uses ONLY .each. Here, I have taken the numbers inside ((array) - 1,2,3) and squared them by first selecting each element individually and storing it in the variable pipes "|num|". Because Ruby willthe Ruby exponent operator **2 
+#This array method uses ONLY .each. Here, I have taken the numbers inside ((array) - 1,2,3) and squared them by first selecting each element individually and storing it in the variable pipes "|num|". Because the return values are not being stored and I can't use .collect in this task, I have created an empty array called squared_numbers which I store/collect them inside after squaring the elements/variables i.e. num using the Ruby exponent operator **2 and then push them to be stored inside of the squared_numbers array, thus capturing the new squared returned values, which should be 2,4 and 9. 
 
 def square_array(array)
   squared_numbers = []
