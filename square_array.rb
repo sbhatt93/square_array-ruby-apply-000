8c37c6dd04e36425af9f9ab5168c2954516343e5
jocular-inspector-4517 <shivbhatt@hotmail.com>
2019-02-26 00:33:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,4 @@
-#This method below uses only the .each array method ONLY. Here, we have our method #square_array which we want to use only .each in the logic to capture the squared numbers/elements (since we can't use .collect, .inject in this method). I have created an empty array named squared_numbers and 
+#This method below uses only the .each array method ONLY. Here, we have our method #square_array which we want to use only the .each in the logic to capture the squared numbers/elements (since we can't use .collect, .inject in this method). I have created an empty array named squared_numbers and 
 
 def square_array(array)
   squared_numbers = []
